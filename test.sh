c6b2dc0fb43255e5f35d0e931aeb0f1a7c0a76fb
andrew23 <andrew23>
2003-03-10 11:36:14 +0000

    upd: test script

--- test.sh
+++ test.sh
@@ -18,20 +18,33 @@ for inputFile in `find ./test -name "*.dxf"`
 do
 	echo "Input: $inputFile"
 	outputFile=${inputFile%.*}
-	echo "Output: $outputFile"
+	echo "  Output: $outputFile.swf"
 	./vec2web $inputFile $outputFile.swf -x 800 -y 600
+	echo "  Output: $outputFile.png"
 	./vec2web $inputFile $outputFile.png -x 800 -y 600
+	#echo "  Output: $outputFile.gif"
 	#./vec2web $inputFile $outputFile.gif -x 800 -y 600
+	echo "  Output: $outputFile.jpeg"
 	./vec2web $inputFile $outputFile.jpeg -x 800 -y 600
+	echo "  Output: $outputFile.xbm"
 	./vec2web $inputFile $outputFile.xbm -x 800 -y 600
+	echo "  Output: $outputFile.xpm"
 	./vec2web $inputFile $outputFile.xpm -x 800 -y 600
+	echo "  Output: $outputFile.pbm"
 	./vec2web $inputFile $outputFile.pbm -x 800 -y 600
+	echo "  Output: $outputFile.pgm"
 	./vec2web $inputFile $outputFile.pgm -x 800 -y 600
+	echo "  Output: $outputFile.ppm"
 	./vec2web $inputFile $outputFile.ppm -x 800 -y 600
+	#echo "  Output: $outputFile.mng"
 	#./vec2web $inputFile $outputFile.mng -x 800 -y 600
+	echo "  Output: $outputFile.bmp"
 	./vec2web $inputFile $outputFile.bmp -x 800 -y 600
+	echo "  Output: $outputFile.ps"
 	./vec2web $inputFile $outputFile.ps -o l -s A4
+	echo "  Output: ${outputFile}_bw.ps"
 	./vec2web $inputFile ${outputFile}_bw.ps -b -o l -s A4
+	echo "  Output: $outputFile.dxml"
 	./vec2web $inputFile $outputFile.dxml -x 800 -y 600
 done
 
